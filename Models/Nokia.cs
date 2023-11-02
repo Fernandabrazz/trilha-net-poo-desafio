4313037c795e6667a7c0dfe22a5288661d63f1f6
Fernanda <fservidorabraz@gmail.com>
2023-11-02 08:14:44 -0300

    Desafio Concluído!

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,32 @@
-namespace DesafioPOO.Models
-{
-    // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+ namespace  DesafioPOO.Models
+
+    // Inherit from the abstract class "Smartphone"
     {
-        // TODO: Sobrescrever o método "InstalarAplicativo"
+ 
+    
+    public class Nokia  : Smartphone
+    {
+    public Nokia (string numero, string modelo, string imei, int memoria) : base (numero,modelo, imei,memoria)
+          {
+
+          }
+
+     
+          // TODO: Sobrescrever o método "InstalarAplicativo"
+     
+       public override void InstalarAplicativo (string nomeApp)
+
+        {
+            Console.WriteLine($"Instalando o aplicativo {nomeApp} no Nokia .");
+                    } 
+
     }
+
 }
+
+ 
--- Program.cs
+++ Program.cs
@@ -1,3 +1,26 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+//TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+ Smartphone nokia = new Nokia(numero: "986543", "modelo: 01", imei: "3333333", memoria:64);
+ nokia.ReceberLigacao();
+ nokia.Ligar();
+ nokia.InstalarAplicativo("Discord");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone");
+ Smartphone iphone = new Iphone(numero: "98565", "modelo: 6", imei: "666666", memoria:128);
+ iphone.Ligar();
+ iphone.ReceberLigacao();
+ iphone.InstalarAplicativo("Github");
+ Console.WriteLine("\n");
+
+
+
